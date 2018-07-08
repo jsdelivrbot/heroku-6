94f9ff26e73a57210e697b9fca1b93c600638e0f
dkndev <13693174+dkndev@users.noreply.github.com>
2018-07-08 18:42:09 +0200

    index update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+
+const http = require('http')
 const PORT = process.env.PORT || 5000
 
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -10,3 +13,19 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+  const requestHandler = (request, response) => {
+	  console.log(request.url)
+	  response.end('Hello Node.js Server!')
+	}
+
+	const server = http.createServer(requestHandler)
+
+	server.listen(PORT, (err) => {
+	  if (err) {
+	    return console.log('something bad happened', err)
+	  }
+
+	  console.log(`server is listening on ${PORT}`)
+	})
