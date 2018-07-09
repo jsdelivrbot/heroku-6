b8c19b36d47f650e5417e33297fd42cd667749f0
dkndev <13693174+dkndev@users.noreply.github.com>
2018-07-09 09:32:07 +0200

    index update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-/*
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -11,17 +11,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
 
-var app = express();
+// var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+// app.get('/', function (req, res) {
+//    res.send('Hello World');
+// })
 
-var server = app.listen(PORT, function () {
-   var host = server.address().address
-   var port = server.address().port
+// var server = app.listen(PORT, function () {
+//    var host = server.address().address
+//    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
